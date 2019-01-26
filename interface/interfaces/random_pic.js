d572fba4738a4727e198374d168083989efc4fc6
MikuBlog <786601672@qq.com>
2019-01-26 19:30:49 +0800

    2019-01-26

---
+++ interface/interfaces/random_pic.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+
+function getPicture(req, res) {
+    var length;
+    new Promise((resolve, reject) => {
+        fs.readdir('./static/images/', (err, files) => {
+            if (err) {
+                reject(err)
+            }
+            length = files.length
+            resolve(length)
+        })
+    }).then((length) => {
+        res.sendFile(path.join(__dirname, `../static/images/${parseInt(Math.random() * length)}.jpg`))
+    }).catch((err) => {
+        res.send({
+            status: "error"
+        })
+    })
+}
+
+module.exports = {
+    getPicture
+} 
--- interface/server.js
+++ interface/server.js
@@ -1,13 +1,19 @@
 require('./database/database_start')
 
 const express = require('express')
+const fs = require('fs')
 const bodyParser = require('body-parser')
 const randomWord = require('./interfaces/random_word')
+const randomPic = require('./interfaces/random_pic')
 const app = express()
+
+app.use(express.static(__dirname+ "/static"))
 
 // 获取随机一言
 app.get('/getWord', randomWord.getRandomWord)
 // 插入随机一言
 app.post('/insertWord', bodyParser.urlencoded({extends: false}), randomWord.insertWord)
+//随机获取一张缩略图
+app.get('/getPicture', randomPic.getPicture)
 
 app.listen(8888)
