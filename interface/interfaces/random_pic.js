4564b60147ce9b27b311ef9aed2f6e0658d127f7
Miku <43233731+MikuBlog@users.noreply.github.com>
2019-01-27 19:15:57 +0800

    Update random_pic.js

--- interface/interfaces/random_pic.js
+++ interface/interfaces/random_pic.js
@@ -3,13 +3,12 @@ const path = require('path')
 
 // 随机请求本地图片
 function getPicture(req, res) {
-    var length;
     new Promise((resolve, reject) => {
         fs.readdir('./static/images/', (err, files) => {
             if (err) {
                 reject(err)
             }
-            length = files.length
+            const length = files.length
             resolve(length)
         })
     }).then((length) => {
