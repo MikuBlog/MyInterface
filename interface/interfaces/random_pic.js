191543819c54e3b0f3a8db8f16089207428e55e4
Xuan Zai <43233731+MikuBlog@users.noreply.github.com>
2019-02-20 11:20:54 +0800

    Update random_pic.js

--- interface/interfaces/random_pic.js
+++ interface/interfaces/random_pic.js
@@ -1,21 +1,36 @@
 const fs = require('fs')
+const url = require('url')
 const path = require('path')
 
 function getPicture(req, res) {
+    // 获取图片类型
+    const getPath = url.parse(req.url).query ? decodeURI(url.parse(req.url).query.split('&')[0].split('=')[1]) : "miku"
     new Promise((resolve, reject) => {
-        fs.readdir('./static/images/', (err, files) => {
+        fs.readdir(`./static/${getPath}/`, (err, files) => {
             if (err) {
                 reject(err)
             }
-            const fileName = files[parseInt(Math.random() * files.length)]
-            resolve(fileName)
+            try {
+            	// 获取文件名称
+            	const fileName = files[parseInt(Math.random() * files.length)]
+            	// 获取文件对应名称
+            	const number = url.parse(req.url).query.split('&')[1] ? decodeURI(url.parse(req.url).query.split('&')[1].split('=')[1]) : ""
+                resolve(number ? (number < files.length ? number + ".jpg" : "error") : fileName)
+            }catch(e) {
+            	reject(e)
+            }
         })
     }).then((fileName) => {
-        res.sendFile(path.join(__dirname, `../static/images/${fileName}`))
+     	// 如果没有该图片，返回错误图片
+    	if(fileName == "error") {
+    		res.sendFile(path.join(__dirname, `../static/error/error.jpg`))
+    		return
+    	}
+    	// 返回图片
+        res.sendFile(path.join(__dirname, `../static/${getPath}/${fileName}`))
     }).catch((err) => {
-        res.send({
-            status: "error"
-        })
+    	// 如果没有该图片，返回错误图片
+        res.sendFile(path.join(__dirname, `../static/error/error.jpg`))
     })
 }
 
