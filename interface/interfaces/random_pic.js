6d1c1a176989ca75b41778d8c5b2b569659a8b8e
Xuan Zai <43233731+MikuBlog@users.noreply.github.com>
2019-02-20 11:53:34 +0800

    2019-02-20

--- interface/interfaces/random_pic.js
+++ interface/interfaces/random_pic.js
@@ -4,9 +4,16 @@ const path = require('path')
 
 function getPicture(req, res) {
     // 获取图片类型
-    const getPath = url.parse(req.url).query ? decodeURI(url.parse(req.url).query.split('&')[0].split('=')[1]) : "miku"
     new Promise((resolve, reject) => {
-        fs.readdir(`./static/${getPath}/`, (err, files) => {
+    		fs.readdir(`./static/`, (err, files) => {
+    			if(err) {
+    				reject(err)
+    			}		
+    			resolve(url.parse(req.url).query ? decodeURI(url.parse(req.url).query.split('&')[0].split('=')[1]) : files[parseInt(Math.random() * files.length)])	
+    		})
+    }).then((getPath) => {
+        return new Promise((resolve, reject) => {
+        	fs.readdir(`./static/${getPath}/`, (err, files) => {
             if (err) {
                 reject(err)
             }
@@ -14,24 +21,32 @@ function getPicture(req, res) {
             	// 获取文件名称
             	const fileName = files[parseInt(Math.random() * files.length)]
             	// 获取文件对应名称
-            	const number = url.parse(req.url).query.split('&')[1] ? decodeURI(url.parse(req.url).query.split('&')[1].split('=')[1]) : ""
-                // 如果不含该图片id，返回错误图片
-                resolve(number ? (number < files.length ? number + ".jpg" : "error") : fileName)
+            	let number
+            	try {
+            		number = url.parse(req.url).query.split('&')[1] ? decodeURI(url.parse(req.url).query.split('&')[1].split('=')[1]) : ""	
+            	}catch(e) {
+            		number = ""
+            	}
+            	resolve({
+            		getPath: getPath,
+            		fileName: number ? (number < files.length ? number + ".jpg" : "error") : fileName
+            	})
             }catch(e) {
             	reject(e)
             }
-        })
-    }).then((fileName) => {
+        })	
+       })
+    }).then((obj) => {
      	// 如果没有该图片，返回错误图片
-    	if(fileName == "error") {
-    		res.sendFile(path.join(__dirname, `../static/error/error.jpg`))
+    	if(obj.fileName == "error") {
+    		res.sendFile(path.join(__dirname, `../error/error.jpg`))
     		return
     	}
     	// 返回图片
-        res.sendFile(path.join(__dirname, `../static/${getPath}/${fileName}`))
+        res.sendFile(path.join(__dirname, `../static/${obj.getPath}/${obj.fileName}`))
     }).catch((err) => {
-    	// 如果没有该图片，返回错误图片
-        res.sendFile(path.join(__dirname, `../static/error/error.jpg`))
+    		// 如果没有该图片，返回错误图片
+        res.sendFile(path.join(__dirname, `../error/error.jpg`))
     })
 }
 
