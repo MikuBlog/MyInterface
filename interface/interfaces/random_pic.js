2df395d5a8ae88f9ebb4af9ff50543784640fa61
Xuan Zai <43233731+MikuBlog@users.noreply.github.com>
2019-02-21 14:40:07 +0800

    2019-02-21

--- interface/interfaces/random_pic.js
+++ interface/interfaces/random_pic.js
@@ -13,39 +13,39 @@ function getPicture(req, res) {
     		})
     }).then((getPath) => {
         return new Promise((resolve, reject) => {
-        	fs.readdir(`./static/${getPath}/`, (err, files) => {
-            if (err) {
-                reject(err)
-            }
-            try {
-            	// 获取文件名称
-            	const fileName = files[parseInt(Math.random() * files.length)]
-            	// 获取文件对应名称
-            	let number
-            	try {
-            		number = url.parse(req.url).query.split('&')[1] ? decodeURI(url.parse(req.url).query.split('&')[1].split('=')[1]) : ""	
-            	}catch(e) {
-            		number = ""
-            	}
-            	resolve({
-            		getPath: 	getPath,
-            		fileName: number ? (number < files.length && number >= 0 ? files[number] : "error") : fileName
-            	})
-            }catch(e) {
-            	reject(e)
-            }
-        	})	
+                fs.readdir(`./static/${getPath}/`, (err, files) => {
+                if (err) {
+                    reject(err)
+                }
+                try {
+                    // 获取文件名称
+                    const fileName = files[parseInt(Math.random() * files.length)]
+                    // 获取文件对应名称
+                    let number
+                    try {
+                        number = url.parse(req.url).query.split('&')[1] ? decodeURI(url.parse(req.url).query.split('&')[1].split('=')[1]) : ""	
+                    }catch(e) {
+                        number = ""
+                    }
+                    resolve({
+                        getPath: getPath,
+                        fileName: number ? (number < files.length && number >= 0 ? files[number] : "error") : fileName
+                    })
+                }catch(e) {
+                    reject(e)
+                }
+            })	
         })
     }).then((obj) => {
      	// 如果没有该图片，返回错误图片
-    		if(obj.fileName == "error") {
-    			res.sendFile(path.join(__dirname, `../error/error.jpg`))
-    			return
-    		}
-    		// 返回图片
+    	if(obj.fileName == "error") {
+    		res.sendFile(path.join(__dirname, `../error/error.jpg`))
+    		return
+    	}
+    	// 返回图片
         res.sendFile(path.join(__dirname, `../static/${obj.getPath}/${obj.fileName}`))
     }).catch((err) => {
-    		// 如果没有该图片，返回错误图片
+    	// 如果没有该图片，返回错误图片
         res.sendFile(path.join(__dirname, `../error/error.jpg`))
     })
 }
